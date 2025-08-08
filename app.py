1a818e4274a3b82a3a9c5b37c37f196166303a61
JoaoPedro20007 silva <joaopedromoreirasilva727@gmail.com>
2025-08-08 11:36:56 +0000

    termino da criação da conexão com o mysql e inicio dos cruds

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # app.py
 
 from db_config import conecter
+from crud import categoria
 
 def main():
     conexao = conecter()
@@ -23,3 +24,17 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+def menu():
+    while True:
+        print("\n=== MENU SGB ===")    
+        print("1. Criar Categoria")  
+        print("2. Listar Categoria") 
+        print("3. Atualizar Categoria") 
+        print("4. Deletar Categoria") 
+        print("0. Sair") 
+        opcao = input("Escola uma opção: ")
+
+        if opcao == "1":
+            nome = input("Nome da categoria: ")
+            descricao = input ("Dascrição")
